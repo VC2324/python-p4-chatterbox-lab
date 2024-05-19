3e0f71c4bdd954494e3006b22dbca7df583d4827
Victor <victorac2324@gmail.com>
2024-05-19 18:43:22 -0400

    finished lab

--- server/app.py
+++ server/app.py
@@ -14,13 +14,49 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
-def messages():
-    return ''
+@app.get('/messages')
+def get_messages():
+    messages = [m.to_dict() for m in Message.query.order_by(Message.created_at).all()]
+    return messages, 200
 
-@app.route('/messages/<int:id>')
-def messages_by_id(id):
-    return ''
+@app.post('/messages')
+def post_messages():
+    new_msg = Message(
+        body =  request.json.get("body"),
+        username = request.json.get('username')
+    )
+    db.session.add(new_msg)
+    db.session.commit()
+
+    return new_msg.to_dict(), 201
+
+
+
+
+@app.patch('/messages/<int:id>')
+def patch_messages_by_id(id):
+    msg_to_patch= Message.query.where(Message.id ==id).first()
+    if msg_to_patch:
+        for key in request.json.keys():
+            if not key == 'id':
+                setattr(msg_to_patch, key, request.json[key])
+        db.session.add(msg_to_patch)
+        db.session.commit()
+        return msg_to_patch.to_dict(), 202
+    else:
+        return {'error' : "Not Found"}, 404 
+    
+@app.delete("/messages/<int:id>")
+def delete_message(id):
+    msg_to_delete= Message.query.where(Message.id == id).first()
+    if msg_to_delete :
+        db.session.delete(msg_to_delete)
+        db.session.commit()
+        return {}, 204
+    else:
+        return {"error" : "Not Found"}, 404
+
+    
 
 if __name__ == '__main__':
     app.run(port=5555)
